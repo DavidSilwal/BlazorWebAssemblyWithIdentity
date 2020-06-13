2323ac9def8bf527b25bf8011e450aebe1c3bc3c
Daniel Roth <daroth@microsoft.com>
2020-06-12 17:41:30 -0700

    Scaffold identity

---
+++ Server/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using BlazorWebAssemblyWithIdentity.Server.Data;
+using BlazorWebAssemblyWithIdentity.Server.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BlazorWebAssemblyWithIdentity.Server.Areas.Identity.IdentityHostingStartup))]
+namespace BlazorWebAssemblyWithIdentity.Server.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
